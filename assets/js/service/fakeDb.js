fc4b6c4eae455fd8957457a3e3f0f976075d4a5e
Faranix <stephane59119@gmail.com>
2022-07-06 14:39:10 +0200

    Ajout d'information dynamique, des images, et une balide meta description

--- assets/js/service/fakeDb.js
+++ assets/js/service/fakeDb.js
@@ -22,73 +22,73 @@ const fakeUser = [
 const fakeDb = [
     {
         title: "HTML5",
-        description: null,
+        description: "Apprenez à coder en HTML pour créer le squelette de votre page web et avec CSS développer votre site web !",
         image: {
-            src: "e-learning.jpg",
-            alt: "Un test"
+            src: "html.jpg",
+            alt: "Un morceau de papier qui montre le langage de balisage HTML."
         },
         href: "#"
     },
     {
         title: "CSS",
-        description: null,
+        description: "Apprenez à coder en CSS pour rajouter du design dans vos sites web, les rendre plus attractif !",
         image: {
-            src: "e-learning.jpg",
-            alt: "Un test"
-        },
-        href: "#"
-    },
-    {
-        title: "Javascript",
-        description: null,
-        image: {
-            src: "e-learning.jpg",
-            alt: "Un test"
+            src: "css.jpg",
+            alt: "Un morceau de papier qui montre le langage CSS pour faire des feuilles de style."
         },
         href: "#"
     },
     {
         title: "NodeJs",
-        description: null,
+        description: "Apprenez à coder en Node JS pour créer votre back-end, votre api rest !",
         image: {
-            src: "e-learning.jpg",
-            alt: "Un test"
-        },
-        href: "#"
-    },
-    {
-        title: "MySql",
-        description: null,
-        image: {
-            src: "e-learning.jpg",
-            alt: "Un test"
-        },
-        href: "#"
-    },
-    {
-        title: "Scss",
-        description: null,
-        image: {
-            src: "e-learning.jpg",
-            alt: "Un test"
+            src: "nodejs.jpg",
+            alt: "Un morceau de papier qui montre le langage Node JS pour créer votre back-end."
         },
         href: "#"
     },
     {
         title: "ReactJs",
-        description: null,
+        description: "Apprenez à coder en React Js pour créer vos interfaces utilisateur rapidement grace à cet outil surpuissant ",
         image: {
-            src: "e-learning.jpg",
-            alt: "Un test"
+            src: "reactjs.jpg",
+            alt: "Un morceau de papier qui montre le framework React JS pour créer vos interfaces rapidement !"
         },
         href: "#"
     },
     {
         title: "VueJs",
-        description: null,
+        description: "Apprenez à coder en Vue JS pour créer un site web rapidement et simplement !",
         image: {
-            src: "e-learning.jpg",
-            alt: "Un test"
+            src: "vuejs.jpg",
+            alt: "Un morceau de papier qui montre le framework Vue JS pour créer vos interfaces rapidement."
+        },
+        href: "#"
+    },
+    {
+        title: "Git",
+        description: "Apprenez Git pour créer des versions de votre code et gérez les imprévues !",
+        image: {
+            src: "git.jpg",
+            alt: "Un morceau de papier qui montre git."
+        },
+        href: "#"
+    },
+    {
+        title: "Npm",
+        description: "Apprenez NPM (Node Package Manager) pour manager vos packages simplement !",
+        image: {
+            src: "npm.jpg",
+            alt: "Un morceau de papier qui montre npm (Node Package Manager) pour gerer vos packages rapidement."
+        },
+        href: "#"
+    },
+    {
+        title: "Json",
+        description: "Apprenez JSON pour gérer vos données simplement et rapidement !",
+        image: {
+            src: "json.jpg",
+            alt: "Un morceau de papier qui montre Json pour manipuler vos données"
         },
         href: "#"
     },
