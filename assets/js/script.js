52d8a6e1f1b2a965a55b3fdc4a07fa6ff29610eb
Faranix <stephane59119@gmail.com>
2022-07-06 14:57:35 +0200

    Correction du bug des chemins

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 import { fakeDb, fakeUser } from "./service/fakeDb.js";
-import Cours from "./class/Cours.js";
-import Auth from "./class/Auth.js";
+import Cours from "/assets/js/class/Cours.js";
+import Auth from "/assets/js/class/Auth.js";
 
 const cours = new Cours();
 cours.createHtmlCours(fakeDb);
