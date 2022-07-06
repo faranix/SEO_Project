941c6e179031229245d8bee5094fc1576c3482fd
Faranix <stephane59119@gmail.com>
2022-07-06 15:34:11 +0200

    Router

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,12 @@ import Auth from "./class/Auth.js";
 
 const loc_array = window.location.pathname.split("/");
 
-console.log(loc_array[loc_array.length - 1]);
-
 switch(loc_array[loc_array.length - 1]) {
     case "": case "/index.html": case "/":
         const cours = new Cours();
         cours.createHtmlCours(fakeDb);
         break;
     default:
-        console.log("Page non trouvée");
         break;
 }
 
