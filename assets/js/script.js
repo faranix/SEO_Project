aa167bb6e6439a06d6fd5801b567a4027cac0b28
Faranix <stephane59119@gmail.com>
2022-07-06 14:53:37 +0200

    Push pour le prof

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,5 @@ import { fakeDb, fakeUser } from "./service/fakeDb.js";
 import Cours from "./class/Cours.js";
 import Auth from "./class/Auth.js";
 
-
-
-const location = window.location.pathname;
-
-switch (location) {
-    case "/index.html":
-        const cours = new Cours();
-        cours.createHtmlCours(fakeDb);
-        break;
-    default: 
-        console.log("Page non trouvée");
-        break;
-}
-console.log(location);
+const cours = new Cours();
+cours.createHtmlCours(fakeDb);
