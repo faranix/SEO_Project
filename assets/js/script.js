f1063fe30d787c8868843e9d76aca82c9e74b4b1
Faranix <stephane59119@gmail.com>
2022-07-06 15:17:32 +0200

    push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 import { fakeDb, fakeUser } from "./service/fakeDb.js";
-import Cours from "/assets/js/class/Cours.js";
-import Auth from "/assets/js/class/Auth.js";
+import Cours from "./class/Cours.js";
+import Auth from "./class/Auth.js";
+
+
+
+const location = window.location;
+
 
 const cours = new Cours();
 cours.createHtmlCours(fakeDb);
